fcd4cc63daf023d62ff9a297dfd1ba2dabf8ad67
Marwin <marwinbr@gmail.com>
2020-11-30 15:05:58 -0300

    pegando os projetos de lei

--- vereadores-atuais.R
+++ vereadores-atuais.R
@@ -12,9 +12,47 @@ partidos <- read_html(u_ver) %>%
   xml_attr("title") %>% 
   str_squish()
 
-ver_atuais <- read_html(u_ver) %>% 
+foto_vereador <- read_html(u_ver) %>% 
+  xml_find_all("//td[@class='td3']//img") %>% 
+  xml_attr("src")
+  
+
+vereadores <- read_html(u_ver) %>% 
   xml_find_first('//table') %>% 
   html_table(header = TRUE) %>% 
   janitor::clean_names() %>% 
   as_tibble() %>% 
-  mutate(partido = partidos)
+  mutate(partido = partidos,
+         foto = foto_vereador)
+
+# Atividade Parlamentar
+
+
+# Projetos de Lei - 2017 a 2020
+# 
+
+# u_atv_par <- "http://www.camara.rj.gov.br/controle_atividade_parlamentar.php?"
+# 
+# q_prolei <- list(
+#   "m1" = "materias_leg",
+#   "m2" = "10a_Leg",
+#   "m3" = "prolei",
+#   "url" =  "http://mail.camara.rj.gov.br/APL/Legislativos/scpro1720.nsf/Internet/LeiInt?OpenForm"
+# )
+
+prolei <- GET("https://mail.camara.rj.gov.br/APL/Legislativos/scpro1720.nsf/Internet/LeiEmentaInt?OpenForm=&Start=1&Count=100&Expand=1")
+
+read_html(prolei) %>% 
+  xml_find_all('//table') %>%
+  magrittr::extract2(2) %>% 
+  html_table(fill = TRUE) %>% 
+  janitor::clean_names() %>% 
+  select(c("x_2", "ementa", "data_publ", "autor_es")) %>% 
+  set_names(c("prolei_num", "ementa", "data_publi", "autores")) %>% 
+  as_tibble() %>% 
+  slice(-c(1,2))
+  
+xml_f
+
+?html_table
+  
